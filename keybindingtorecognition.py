76f3ccf254de56e87ff3d240e58db8f431973fec
Erilysse <estelle.manic@gmail.com>
2018-11-20 23:53:11 +0900

    Update keybindingtorecognition.py
    Change use of pyautogui rather than tkinter
    bind key with movement and gesture pose on camera for differents actions on internet navigator

--- keybindingtorecognition.py
+++ keybindingtorecognition.py
@@ -1,5 +1,37 @@
-from tkinter import *
+from pyautogui import *
 import numpy as m
 from object_movement import dirX, dirY
 
-(dirX, dirY) 
+#left and right movement using direction information
+#left means going to the left tab on internet
+#right means going to the right tab on internet
+def f(dirX):
+    return {
+        'Left':
+        pyautogui.hotkey('ctrl', 'shift', 'tab')
+        break
+        'Right':
+        pyautogui.hotkey('ctrl', 'tab')
+        break
+    }[dirX]
+}
+
+#close or far away the camera using depth information
+#close means open a new tab on internet
+#far means close the current tab on internet
+def f(dirD):
+    return {
+        'Close':
+        pyautogui.hotkey('ctrl', 't')
+        break
+        'Far':
+        pyautogui.hotkey('ctrl', 'w')
+        break
+    }[dirD]
+}
+
+#doing a punch with gesture recognition
+#you can change between applications
+if gesture== "punch"
+    pytaugui.hotkey('alt','tab')
+    break
